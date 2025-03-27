54635df38b4cf93dd3e2cb7fe882d404bf4a3969
Matvey <matvey.ivchenko@yandex.ru>
2025-03-27 22:52:53 +0300

    ещё причесал

--- board2vec/main.py
+++ board2vec/main.py
@@ -1,8 +1,22 @@
-# from experiments.experiment_03.inference import board2vec
-# from experiments.utils.vizualize import main
+from experiments.experiment_04.inference import board2vec
+from experiments.utils.vizualize import main
+import pandas as pd
+import chess
 
-# main(board2vec)
+# Таблицу можно скачать здесь: https://disk.yandex.ru/d/q1G0W-taR0TSxw
+data_path = 'C:/Users/matvey/Documents/chess_data/full_labeled.csv'
 
-from experiments.experiment_02.train import run_train
+data = pd.read_csv(data_path)
+boards = []
+for game in data['moves'].str.split().head(100):
+    board = chess.Board()
+    for move in game:
+        board.push(chess.Move.from_uci(move))
+        boards.append(board.copy())
 
-run_train()
+# Визуализируем
+main(boards, board2vec(boards))
+
+
+# from experiments.experiment_02.train import run_train
+# run_train()
